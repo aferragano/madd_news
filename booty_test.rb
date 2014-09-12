0da22592f8075ae50b5719522d86df1342746e6e
Katherine <kdmcclin@gmail.com>
2014-09-12 09:50:13 -0400

    Testing booty algorithm.

---
+++ booty_test.rb
@@ -0,0 +1,16 @@
+string = "Oscar Pistorius, the disabled track star who once commanded stellar heights of international competition at the Paralympic and Olympic Games, was found guilty on Friday of culpable homicide, equivalent to manslaughter, after being acquitted of murder charges for killing his girlfriend."
+
+news = string.split
+
+butt = ["booty", "ass", "arse", "bum", "keister", "tooshie"]
+
+def bootify(string_array, word_array)
+ string_array.each_with_index do |string, index|
+    if string.match(/\A[A-Z]+/)
+      string_array[index] << " " + word_array.sample
+    end
+  end
+  string_array.join(" ")
+end
+
+p bootify(news, butt)
