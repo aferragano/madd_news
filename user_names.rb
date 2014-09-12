3f19e5b768c6681f38d9f38bde382fb475968aca
Mark <markdmcq@gmail.com>
2014-09-12 15:03:44 -0400

    Adjective and Noun Arrays

---
+++ user_names.rb
@@ -0,0 +1,6 @@
+
+
+
+adjectives = ["Killer", "Bloody", "Schweaty", "Fresh", "Rancid", "Ferocious", "Stupendous", "Slippery", "Deep", "Courteous", "Aromatic", "Tenacious", "Mega", "Impermanent", "Sticky", "Moist", "Rabid", "Reigning", "Purplish", "Suspicious", "Hairy", "Lethargic", "Problematic", "Enlightened", "Filthy", "Squeaky", "Supersonic", "Soapy", "Transparent", "Hypothetical", "Joyous", "Fantabulous", "Garalous", "Talkative", "Geriatric", "Constipated", "Fungal", "Ostentious", "Compromised", "Subliminal", "Freakadelic", "Fuzzy", "Moldy", "Jovial"]
+
+nouns = ["Bathroom", "Stop Sign", "Black Widow", "Boogers", "Slugs", "Pumpkins", "Napkin", "Kitten", "Bling", "Zucchini", "Submarine", "Tomato", "Nuts", "Raisins", "Wunderkind", "Apricot", "Toe", "Saliva", "Caterpillar", "Mud Turtle", "Creature Feature", "Chihuahua", "Cactus", "Mexico", "Rhubbard", "Fertilization", "Traffic", "Sandpaper", "Spaghetti", "Taco", "Comrade", "Ligtning", "Pimple", "Snot", "Fingernail", "Bubalicious", "Goatee", "Worms", "Gerbil", "Handshake", "Evangelist", "Jawa", "Pokemon", "Riptide," "Laundry", "Broadway Musical"   ]
