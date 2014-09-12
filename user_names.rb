0652812890974280b618ab1c456930ea5df7efe2
Mark <markdmcq@gmail.com>
2014-09-12 15:48:16 -0400

    Added 3 Topic Arrays

--- user_names.rb
+++ user_names.rb
@@ -1,6 +1,15 @@
-
-
-
+#FOR USERNAMES
 adjectives = ["Killer", "Bloody", "Schweaty", "Fresh", "Rancid", "Ferocious", "Stupendous", "Slippery", "Deep", "Courteous", "Aromatic", "Tenacious", "Mega", "Impermanent", "Sticky", "Moist", "Rabid", "Reigning", "Purplish", "Suspicious", "Hairy", "Lethargic", "Problematic", "Enlightened", "Filthy", "Squeaky", "Supersonic", "Soapy", "Transparent", "Hypothetical", "Joyous", "Fantabulous", "Garalous", "Talkative", "Geriatric", "Constipated", "Fungal", "Ostentious", "Compromised", "Subliminal", "Freakadelic", "Fuzzy", "Moldy", "Jovial"]
 
-nouns = ["Bathroom", "Stop Sign", "Black Widow", "Boogers", "Slugs", "Pumpkins", "Napkin", "Kitten", "Bling", "Zucchini", "Submarine", "Tomato", "Nuts", "Raisins", "Wunderkind", "Apricot", "Toe", "Saliva", "Caterpillar", "Mud Turtle", "Creature Feature", "Chihuahua", "Cactus", "Mexico", "Rhubbard", "Fertilization", "Traffic", "Sandpaper", "Spaghetti", "Taco", "Comrade", "Ligtning", "Pimple", "Snot", "Fingernail", "Bubalicious", "Goatee", "Worms", "Gerbil", "Handshake", "Evangelist", "Jawa", "Pokemon", "Riptide," "Laundry", "Broadway Musical"   ]
+nouns = ["Bathroom", "Stop Sign", "Black Widow", "Boogers", "Slugs", "Pumpkins", "Napkin", "Kitten", "Bling", "Zucchini", "Submarine", "Tomato", "Nuts", "Raisins", "Wunderkind", "Apricot", "Toe", "Saliva", "Caterpillar", "Mud Turtle", "Creature Feature", "Chihuahua", "Cactus", "Mexico", "Rhubbard", "Fertilization", "Traffic", "Sandpaper", "Spaghetti", "Taco", "Comrade", "Ligtning", "Pimple", "Snot", "Fingernail", "Bubalicious", "Goatee", "Worms", "Gerbil", "Handshake", "Evangelist", "Jawa", "Pokemon", "Riptide," "Laundry", "Broadway Musical"]
+
+
+
+#TOPIC CHOICES
+
+mechanic = ["crank-shaft", "carburetor", "coil-spring", "drive-shaft", "fly-wheel", "lubrication", "piston", "piston-rod", "spark-plugs", "speedometer", "tire-jack", "torque-wrench"]
+
+monty_python = ["dead parrot", "lumberjack", "spam", "sit on my face", "bring out your dead", "Ni!", "Bruces", "the holy hand grenade", "Finland", "wafer thin mint", "every sperm is sacred", "unladen swallow", "half-a-bee"]
+
+mcdonalds = ["Big Mac", "McDouble", "McChicken", "McRib", "Mac Snack Wrap", "McMuffin", "McCafe Latte", "McNuggets"]
+
