53e1faa741fffbe4603ba685901d05ec1ef92381
Katherine <kdmcclin@gmail.com>
2014-09-12 10:29:32 -0400

    Fixed insertion issues.

--- booty_test.rb
+++ booty_test.rb
@@ -1,6 +1,8 @@
+require 'pry'
+
 string = "Oscar Pistorius, the disabled track star who once commanded stellar heights of international competition at the Paralympic and Olympic Games, was found guilty on Friday of culpable homicide, equivalent to manslaughter, after being acquitted of murder charges for killing his girlfriend."
 
-news = string.split
+news = string.gsub(",", " ,").split
 
 butt = ["booty", "ass", "arse", "bum", "keister", "tooshie"]
 
@@ -10,7 +12,7 @@ def bootify(string_array, word_array)
       string_array[index] << " " + word_array.sample
     end
   end
-  string_array.join(" ")
+  string = string_array.join(" ").gsub(" ,", ",")
 end
 
 p bootify(news, butt)
