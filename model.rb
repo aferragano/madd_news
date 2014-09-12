f2d3381ae649c162aae25712e7ebf5419c925b58
Anthony Ferragano <aferragano@gmail.com>
2014-09-12 08:06:38 -0400

    created the general controller, view. need more methods to play with in model

--- model.rb
+++ model.rb
@@ -2,6 +2,32 @@ class Model
 
 	def initialize
 
+		@words = []
+		@url = 
+
+
+
 	end
+
+	def words_to_implement(word_selection)
+
+		case word_selection
+		when "1"
+			words_to_use = #that constant
+		when "2"
+			words_to_use = #that constant2
+		when "3"
+			words_to_use = #that constant2
+		end
+
+		words_to_use
+
+	end
+
+	def make_madd_news(paragraph, words_to_use)
+
+	end
+
+
 
 end
--- view.rb
+++ view.rb
@@ -1,6 +1,7 @@
 require_relative 'model.rb'
 require_relative 'parser.rb'
 
+#CONSTANT WORDS ARRAY...
 
 class View
 
@@ -14,21 +15,58 @@ class View
 		puts "\n" * 5
 	end
 
+	def prompt_user_name
+		puts 
+		puts "\t Please enter your name:"
+		puts
+		name = gets.chomp
+	end
+
 	def input_url_prompt
 		puts 
 		puts "\t Copy & Paste your selected news article URL"
 		puts
 	end
 
-	def select_words_prompt
+	def get_url
+		gets.chomp
+	end
+
+	def nice_choice
 		puts 
-		puts "\t Please select which words you selected"
+		puts "\t Nice choice"
 		puts
 	end
 
-	def display_madd_news(madd_news)
+	def select_words_prompt
 		puts 
+		puts "\t Please identify which words you wish to implement in your news article"
+		puts
+	end
+
+	def display_word_options
+		puts 
+		puts "\t 1. #{}"
+		puts "\t 2. #{}"
+		puts "\t 3. #{}"
+		puts
+		gets.chomp
+	end
+
+	def display_madd_news(madd_news)
+		puts "YOUR MADD NEWS"
+		puts "=" * 50
+		puts
 		puts "\t #{madd_news}"
+		puts
+		puts "=" *50
+		sleep(0.9999)
+	end
+
+
+	def thank_user_prompt(user_name)
+		puts
+		puts "\n\tThanks you #{user_name} for using MADD NEWS\n\n We hope you enjoyed your new news."
 		puts
 	end
 
