b2cc634d5a393434fbff7857a194d396d4f14b08
Katherine <kdmcclin@gmail.com>
2014-09-12 14:40:52 -0400

    partial complete

--- booty_test.rb
+++ booty_test.rb
@@ -2,15 +2,17 @@ require 'pry'
 
 string = "Oscar Pistorius, the disabled track star who once commanded stellar heights of international competition at the Paralympic and Olympic Games, was found guilty on Friday of culpable homicide, equivalent to manslaughter, after being acquitted of murder charges for killing his girlfriend."
 
-news = string.gsub(",", " ,").split
+def convert_string(string)
+  news = string.gsub(",", " ,").split
+end
 
-butt = ["booty", "ass", "arse", "bum", "keister", "tooshie", "badonkadonk", "fanny", "money-maker"]
+butt = ["booty", "ass", "arse", "bum", "keister", "tooshie", "badonkadonk", "fanny", "money-maker", "fatty"]
 marijuana = ["pot", "weed", "ganja", "homegrown", "laughing grass", "Mary Jane", "Texas tea"]
 miscellaneous = ["blunderbuss", "bowyang", "cockamamie", "eructation", "fard", "fartlek", "widdershins", "smellfungus", "wabbit"]
 drunk = ["shitfaced", "blitzed", "blotto", "pissed", "sauced", "slagged", "trashed"]
 yiddish = ["bopkis", "huppitzville", "farbisene", "farkakt", "kakameyne", "klots", "shlemiel"]
 
-def bootify(string_array, word_array)
+def bootify(string_array, topic)
   completed_array = string_array.dup
   counter = 0
   string_array.each_with_index do |string, index|
@@ -19,7 +21,12 @@ def bootify(string_array, word_array)
       counter += 1
     end
   end
+  word_array.each do|entry|
+    if entry == completed_array[0]
+      completed_array.shift
+    end
+  end
   completed_array.join(" ").gsub(" ,", ",")
 end
 
-p bootify(news, butt)
+p bootify(news, marijuana)
